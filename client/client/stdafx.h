89ed59af6cca248572d2d5095a5a45fd3f494b00
TonyLau <m15611877789@163.com>
2018-11-08 23:48:45 +0800

    first commit

---
+++ client/client/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+// stdafx.cpp : source file that includes just the standard includes
+//	client.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
---
+++ server/server/server.h
@@ -0,0 +1,19 @@
+#pragma once
+#define MAX_CLIENT  10						//同时服务的client数目上限
+#define MAX_BUF_SIZE 65535					//缓存区的大小
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS
+const u_short UDPSrvPort = 2345;			//Server的UDP端口
+const char START_CMD[] = "START";
+const char GETCURTIME_CMD[] = "GET CUR TIME";
+
+//传递给TCP线程的结构化参数
+struct TcpThreadParam
+{
+	SOCKET socket;
+	sockaddr_in addr;
+};
+
+DWORD WINAPI TcpServeThread(LPVOID lpParam);	//TCP线程的线程函数
+DWORD WINAPI UdpServer(LPVOID lpParam);	//UDP服务器线程
+
